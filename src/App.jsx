0bfca1e95895d5507e43aba4b0e26995d09d9ee7
Vitali <vitali276@yandex.ru>
2022-11-25 21:10:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Content from './components/Content';
+
+function App() {
+
+    return (
+        <div>
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    );
+
+}
+
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+    return (
+        <div className="content">
+            <h3>Hello!</h3>
+        </div>
+    );
+}
+
+export default Content;
